4eff817aaed5bc1d459b7ea537310b51e3db764f
Risal Walangadi <risal.walangadi@gmail.com>
2021-05-21 14:26:13 +0800

    add unlike restaurant integration test

---
+++ specs/unlikeRestaurantSpec.js
@@ -0,0 +1,33 @@
+import FavoritesIdb from '../src/scripts/data/favorites-idb';
+import * as TestFactories from './helpers/testFactories';
+
+describe('Unliking A Restaurant', () => {
+  const addLikeButtonContainer = () => {
+    document.body.innerHTML = '<div id="likeButtonContainer"></div>';
+  };
+
+  beforeEach(async () => {
+    addLikeButtonContainer();
+    await FavoritesIdb.putRestaurant({ id: 1 });
+  });
+
+  afterEach(async () => {
+    await FavoritesIdb.deleteRestaurant(1);
+  });
+
+  it('should display unlike button when the restaurant has been liked', async () => {
+    await TestFactories.createLikeButtonPresenterWithRestaurant({ id: 1 });
+
+    expect(
+      document.querySelector('[aria-label="unlike this restaurant"]')
+    ).toBeTruthy();
+  });
+
+  it('should not display like button when the restaurant has been liked', async () => {
+    await TestFactories.createLikeButtonPresenterWithRestaurant({ id: 1 });
+
+    expect(
+      document.querySelector('[aria-label="like this restaurant"]')
+    ).toBeFalsy();
+  });
+});
