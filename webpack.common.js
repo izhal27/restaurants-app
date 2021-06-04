fc5c267f43baef515c0551ad14fe3da3d4243486
Risal Walangadi <risal.walangadi@gmail.com>
2021-06-04 14:07:18 +0800

    disable bundle analyzer plugin open analyzer in browser

--- webpack.common.js
+++ webpack.common.js
@@ -80,6 +80,9 @@ module.exports = {
         }),
       ],
     }),
-    new BundleAnalyzerPlugin(),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+    }),
   ],
 };
